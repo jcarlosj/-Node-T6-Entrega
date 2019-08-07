76f47d848eaef1917c642f713d1eb3f6140b1a29
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2019-08-07 08:53:40 -0500

    [+] Lee fichero 'index.html'

--- taller-1.js
+++ taller-1.js
@@ -1,8 +1,15 @@
-const http = require( 'http' );
+/** Importa librerias de Node */
+const http = require( 'http' ),
+      fs = require( 'fs' );
 
 /** Crea Servidor */
 http. createServer( ( request, response ) => {
     console .log( ` > Running Node Server...` );
+
+    /** Lee fichero index.html de forma Asincrona */
+    fs .readFile( 'index.html', ( error, dataFile ) => {
+        console .log( dataFile .toString() );
+    });
 
     response .writeHead( 200, { 'Content-Type': 'text/plain' } );
     response .end( 'Running Node Server...' );
